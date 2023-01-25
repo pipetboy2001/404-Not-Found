273d8210e1b27f767569830a6e673c0a1fe9487a
Pipetboy <felipefelizx@gmail.com>
2023-01-25 19:40:30 -0300

    Fix: Sass + Typecript

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import React from "react";
-import "./styles/app.scss";
-import twitter from "icons/twitter.svg?url";
+import './Styles/_main.scss'
+function App() {
 
-const App: React.FC = ()=> {
-	return (
-		<>
-			<div className="container">
-				<h1>Hello</h1>
-				<div className="hero">
-					<img src={twitter} alt="" />
-				</div>
-			</div>
-		</>
-	);
-};
+  return (
+    <div>
+      <h2>Hola mundo</h2>
+    </div>
+  )
+}
 
-export default App;
+export default App
