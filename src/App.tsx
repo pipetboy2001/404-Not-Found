1262a1b11fdd14eb1dcbb89e77c59481f49a7fad
Pipetboy <felipefelizx@gmail.com>
2023-01-25 21:15:14 -0300

    Feature: First steps of sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import './Styles/_main.scss'
+import './Styles/main.scss'
+
 function App() {
 
   return (
+    <>
     <div>
-      <h2>Hola mundo</h2>
+      <img src='./assets/Scarecrow.png' alt='Scarecrow'/>
     </div>
+
+    <div>
+      <h1 className='TextBad'>
+        I have bad news for you
+      </h1>
+      <p className='MiniText'>
+        The page you are looking for might be removed or is temporaly unavabiable
+      </p>
+    </div>
+
+    <div>
+      <button className='BadButton'>
+        Back to HomePage
+      </button>
+    </div>
+
+    </>
   )
 }
 
